9ff56582eb215c0a632e3d2a15084d02a386533c
user <user@70802-PC22>
2020-10-12 16:53:26 +0800

    session好難cookie好吃 全部logout

--- src/main/java/lendle/courses/network/sessiontracking/SaveSessionServlet.java
+++ src/main/java/lendle/courses/network/sessiontracking/SaveSessionServlet.java
@@ -11,12 +11,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author lendle
  */
-@WebServlet(name = "SaveSessionServlet", urlPatterns = {"/saveSession"})
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
 public class SaveSessionServlet extends HttpServlet {
 
     /**
@@ -34,6 +35,10 @@ public class SaveSessionServlet extends HttpServlet {
         request.setCharacterEncoding("utf-8");
         //建立 session 並且儲存
         ///////////////////////////////////////////////////////////////
+        HttpSession session = request.getSession();
+        session.invalidate();
+ //       String food=request.getParameter("food");
+ //       session.setAttribute("food", food);
         response.sendRedirect("session.jsp");
     }
 
